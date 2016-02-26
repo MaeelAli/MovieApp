10be9ed47ebbc30ddd0583677a34e3f374c9fbd8
MaeelAli <maeelali92@gmail.com>
2016-02-26 05:35:46 +0000

    developed actors#index route, controller action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: :movie
+
+   get 'actors' => 'actors#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

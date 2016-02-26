2c05e190335fb6e66bafadede56f7e5e297a9ab3
MaeelAli <maeelali92@gmail.com>
2016-02-26 05:21:51 +0000

    developed movies#show route, controller action and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
 	def index
   	@movies = Movie.all
   end
+
+  def show
+    # find movie by its id
+    @movie = Movie.find(params[:id])
+    
+    # retrive all actors of that movie
+    @actors = @movie.actors
+  end
 end
